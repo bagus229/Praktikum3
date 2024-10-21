cfe7e57050b5d4149c21d9a62238c46c7a6a9dce
Bagus Aditya Hermawan <bagus.aditya.h.06@gmail.com>
2024-10-21 18:19:22 +0700

    menambahkan file

--- Flowchart.py
+++ Flowchart.py
@@ -1,22 +1,3 @@
-# Input tiga bilangan
-A = float(input("Masukkan bilangan A: "))
-B = float(input("Masukkan bilangan B: "))
-C = float(input("Masukkan bilangan C: "))
-# Bandingkan bilangan A dan B
-if A > B:
-    # Jika A > B, bandingkan A dengan C
-    if A > C:
-        print(f"Output: Bilangan terbesar adalah A = {A}")
-    else:
-        print(f"Output: Bilangan terbesar adalah C = {C}")
-else:
-    # Jika B >= A, bandingkan B dengan C
-    if B > C:
-        print(f"Output: Bilangan terbesar adalah B = {B}")
-    else:
-        print(f"Output: Bilangan terbesar adalah C = {C}")
-
-
 # Inisialisasi nilai terbesar
 terbesar = float('-inf')  # Nilai awal untuk perbandingan
 
